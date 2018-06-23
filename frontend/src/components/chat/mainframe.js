e5a4e4cf17a9e5e2b6d887a44e97435448702d98
pbdgk <gordonworkspace@gmail.com>
2018-06-23 12:13:01 +0300

    refactored

--- frontend/src/components/chat/mainframe.js
+++ frontend/src/components/chat/mainframe.js
@@ -2,17 +2,14 @@ import React from 'react';
 import Moment from "moment";
 import shortid from "shortid";
 
+import {BrowserRouter as Router, Route } from 'react-router-dom'
 
-import {BrowserRouter as Router, Link, Route } from 'react-router-dom'
-
-
-import PropTypes from "prop-types";
 import DataProvider from "../DataProvider";
 
+import WSocketBox from "./wsbox";
 
-
-import MessageBox from "./message";
-import RoomBar from "./roombar"
+import { MessageBody } from "./message";
+import RoomBar from "./RoomBar"
 
 
 const uuid = shortid.generate;
@@ -20,151 +17,188 @@ const uuid = shortid.generate;
 
 
 class ChatMessage extends React.Component{
-    render(){
-        return(
-                <div>
-                    <div className='chat-message'>
-                        <MessageBox value={this.props.value}/>
-                    </div>
-                </div>
-        );
-    };
+  render(){
+    return(
+      <div>
+        <div className='chat-message'>
+          <MessageBox value={this.props.value}/>
+        </div>
+      </div>
+    );
+  };
 };
 
 
 class ChatInputBox extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+        message: ''
+    };
+    this.update = this.update.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.ref = React.createRef();
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            message: ''
-        };
-        this.update = this.update.bind(this);
-        this.onChange = this.onChange.bind(this);
-        this.ref = React.createRef();
+  update(){
+    let { message } = this.state;
+    message = message.trim()
+    if (message !== '') {
+      this.props.liftMessageToWebSocket({message: message, id: this.props.roomId});
+      this.ref.current.value = '';
     }
+  }
 
-    update(){
-        let message = this.state.message;
-        {
-            message = message.trim()
-            if (message !== '')
-            {
-                this.props.updateFn(message);
-                this.ref.current.value = '';
-            }
-        }
-    }
+  onChange(event){
+    this.setState({ message: event.target.value})
+  }
 
-    onChange(event){
-        this.setState({message: event.target.value})
-    }
-
-    render(){
-        return(
-            <div className='chat-bottom-area'>
-                <div className="chat-input-area">
-                    <textarea ref={this.ref} onChange={this.onChange} id='sendMessage'/>
-                    <button onClick={this.update}/>
-                </div>
-            </div>
-        )
-    }
+  render(){
+    return(
+      <div className='chat-bottom-area'>
+        <div className="chat-input-area">
+          <textarea ref={this.ref} onChange={this.onChange} id='sendMessage'/>
+          <button onClick={this.update}/>
+        </div>
+      </div>
+    )
+  }
 }
 
+const ChatBar = ({ value }) => (
+      <div className='chat-messages-wrapper'>
+        {value.map(msg => (
+          <MessageBody key={uuid()} msg={ msg }/>
+        ))}
+      </div>
+    )
 
-class ChatBar extends React.Component{
-    render(){
-        let data = this.props.value;
-        return(
-            <div className='chat-messages-wrapper'>
-                {data.map(el => (
-                    <ChatMessage key={uuid()} value={el}/>
-                ))}
-            </div>
-        )
-    }
-};
-
-class Profile extends React.Component {
-  state = {
+class Chat extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
       path: null,
       data: null,
       isFetched: false,
-      profileOrError: null,
-  }
+    }
+    this.updateState = this.updateState.bind(this);
+  };
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.match.params.handle !== prevState.path) {
+    if (nextProps.match.params.id !== prevState.path) {
       return {
-        path: nextProps.match.params.handle,
+        path: nextProps.match.params.id,
         data: null,
         isFetched: false,
       };
     }
     return null;
-    }
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.isFetched === false) {
-      // At this point, we're in the "commit" phase, so it's safe to load the new data.
-        this.fetchData(this.state.path);
-    }
   }
-    componentDidMount () {
-        const { handle } = this.props.match.params
-        this.fetchData(handle)
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.isFetched === false) {
+      let { path } = this.state;
+      this.fetchData(path);
     }
 
-    fetchData(handle){
-        fetch(`/api/messages/${handle}`)
-          .then((res) => {return res.json()})
-          .then((data => {
-              this.setState({ data: data, isFetched: true});
-      }))
+    let { data, isUpdated} = this.state
+    let { newMessage } = this.props
+    if (newMessage !== prevProps.newMessage && newMessage !== null){
+      this.setState({data: [ ...data, newMessage ]})
     }
+
+  }
+
+  updateState(message){
+    this.state({data: [...data, message]})
+  }
+
+  componentDidMount () {
+      const { id } = this.props.match.params
+      this.fetchData(id)
+  }
+
+  fetchData(id){
+    fetch(`/api/v1/messages/${id}`)
+      .then((res) => { return res.json()} )
+      .then((data => { this.setState({ data: data, isFetched: true}) }))
+  }
+
   render() {
-      if (!this.state.isFetched && !this.state.data){
+    if (!this.state.isFetched && !this.state.data){
+    return(
+      <div className="col-sm">
+        <ChatInputBox/>
+      </div>
+    )
+    } else{
+      let {liftMessageToWebSocket} = this.props
       return(
-        <div className="col-sm">
-            <div className='wrapper-bar'>
-            </div>
-            <ChatInputBox updateFn={this.props.updateFn}/>
+      <div className="col-sm">
+        <div className='wrapper-bar'>
+          <ChatBar value={this.state.data}/>
         </div>
-      )
-      } else{
-        return(
-        <div className="col-sm">
-                 <div className='wrapper-bar'>
-                     <ChatBar value={this.state.data}/>
-                </div>
-                <ChatInputBox updateFn={this.props.updateFn}/>
-        </div>
-            )
-        }
-    }
+        <ChatInputBox roomId={ this.state.path } liftMessageToWebSocket={liftMessageToWebSocket}/>
+      </div>
+          )
+      }
+  }
 }
 
 
 class MainFrame extends React.Component{
-    state = {
-        userId: window.django.userId,
-    };
-    render(){
-        let roomsUrl = `/api/room_list/${this.state.userId}`;
-        return(
-            <Router>
-                <div className="container main-frame">
-                    <div className="row">
-                        <DataProvider endpoint={roomsUrl}
-                            render={data => <RoomBar value={data} />}/>
-                        <Route exact path='/chat/' render={() => (<div>Choose your contact to start message</div>)} />
-                        <Route path='/chat/:handle' component={Profile} />
-                    </div>
-                </div>
-            </Router>
-        )
+  constructor(props){
+    super(props)
+    this.state = {
+      userId: window.django.userId,
+      newMessage: null,
     }
-}
+    this.ws = this.addWs()
+    this.handleUpdate = this.handleUpdate.bind(this)
+    this.updateState = this.updateState.bind(this)
+  }
 
+  componentWillUnmount() {
+    this.ws.close()
+  }
+
+  handleUpdate(message){
+    this.ws.send(JSON.stringify(message))
+  }
+
+  updateState(message){
+    this.setState({newMessage: message})
+  }
+
+  addWs = () => {
+    const wsUrl = `ws://${window.location.host}/ws/chat/`;
+    let ws = new WebSocket(wsUrl)
+    ws.onopen = e => console.log(e);
+    ws.onmessage = e => { this.updateState(JSON.parse(e.data).message) }
+    ws.onerror = e => console.log('error');
+    ws.onclose = e => console.error('Chat socket closed unexpectedly');
+    return ws;
+  };
+
+
+  render(){
+    let roomsUrl = `/api/v1/rooms/${this.state.userId}/`;
+    let { newMessage } = this.state;
+    return (
+      <Router>
+        <div className="container main-frame">
+          <div className="row">
+            <DataProvider newMessage={ newMessage } endpoint={roomsUrl}
+              render={data => <RoomBar rooms={data} />}/>
+            <Route exact path='/chat/' render={() => (<div>Choose your contact to start message</div>)} />
+            <Route path='/chat/:id' render={(props) => (
+              <Chat {...props} newMessage={ newMessage } liftMessageToWebSocket={ this.handleUpdate }/>
+            )} />
+          </div>
+        </div>
+      </Router>
+    )
+  };
+}
 
 export default MainFrame;
--- frontend/src/components/chat/message.js
+++ frontend/src/components/chat/message.js
@@ -1,31 +1,32 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import Moment from "moment";
-import shortid from "shortid";
-
-import PropTypes from "prop-types";
-
-const uuid = shortid.generate;
 
 
+export const MessageHeader = ({ username }) => (
+  <div className="card-header">
+    <div className='username'>{ username }</div>
+  </div>
+)
+export const MessageBody = ({ msg }) => (
+  <div className="card-body text-primary">
+    <div className='space-btw-wrapper'>
+      <div className='card-left'>
+        <img src='../../../static/frontend/img/tmp.jpeg' className='contact-img-wrapper rounded-circle border border-primary'></img>
+      </div>
+      { msg ? <Message value={ msg }/> : null}
+    </div>
+  </div>
+)
 
-class MessageBox extends React.Component{
-    render(){
-        var dt = this.props.value.send_time ?
-            Moment(this.props.value.send_time).format('HH:mm') : ''
-        return(
-            <div className="card-body text-primary">
-            <div className='space-btw-wrapper'>
-            <div className='card-left'>
-            <img src='../../../static/frontend/img/tmp.jpeg' className='contact-img-wrapper rounded-circle border border-primary'></img>
-            </div>
-            <div className='card-center'><p className="card-text">{this.props.value?this.props.value.msg:''}</p></div>
-            <div className='card-right'>{dt}</div>
-            </div>
-            <hr/>
-            </div>
-        )
-    }
+class Message extends Component{
+  render(){
+    let { msg, send_time } = this.props.value
+    return(
+      <React.Fragment>
+        <div className='card-center'><p className="card-text">{ msg }</p></div>
+        <div className='card-right'>{ Moment(send_time).format('HH:mm') }</div>
+      </React.Fragment>
+    )
+  }
 }
-
-export default MessageBox;
