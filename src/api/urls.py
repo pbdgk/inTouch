5fa4ddacd5cf6c09fe5443fcae9c06ff357229d4
pbdgk <gordonworkspace@gmail.com>
2018-07-06 19:08:21 +0300

    created v1 api app created auth endpoints

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,22 +1,23 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import (
-    Messages,
-    Rooms,
-    RoomsCreate,
-    Contacts,
-    ContactsConnect,
-    Profile,
-    ProfileTemp,
-)
+# from .views import (
+#     Messages,
+#     Rooms,
+#     RoomsCreate,
+#     Contacts,
+#     ContactsConnect,
+#     Profile,
+#     ProfileTemp,
+# )
 
 
 urlpatterns = [
-    path('messages/<str:room_id>/', Messages.as_view()),
-    path('rooms/<int:pk>/', Rooms.as_view()),
-    path('rooms/create/<int:pk>/', RoomsCreate.as_view()),
-    path('contacts/<int:pk>/', Contacts.as_view()),
-    path('contacts/add/<int:pk>/', ContactsConnect.as_view()),
-    path('profiles/<int:pk>/', Profile.as_view()),
-    path('profiles/upload_image/<int:pk>/', ProfileTemp.as_view())
+    path('v1/', include('api.v1.urls')),
+    # path('messages/<str:room_id>/', Messages.as_view()),
+    # path('rooms/<int:pk>/', Rooms.as_view()),
+    # path('rooms/create/<int:pk>/', RoomsCreate.as_view()),
+    # path('contacts/<int:pk>/', Contacts.as_view()),
+    # path('contacts/add/<int:pk>/', ContactsConnect.as_view()),
+    # path('profiles/<int:pk>/', Profile.as_view()),
+    # path('profiles/upload_image/<int:pk>/', ProfileTemp.as_view())
 ]
